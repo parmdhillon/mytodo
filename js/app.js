3052e22474f5d674305390a605e92f5e5d8a650a
Parminder <iparm1996@gmail.com>
2020-09-17 14:23:46 -0400

    Added Task Template and Switchers

--- js/app.js
+++ js/app.js
@@ -1,15 +1,29 @@
 const switchBtnActive = document.getElementById('switchBtnActive');
 const switchBtnCompleted = document.getElementById('switchBtnCompleted');
+const taskEl = document.querySelectorAll('.task--wrapper');
 
 // Toggles Tasks on Mobile Devices
 const toggleTask = () => {
   if (window.matchMedia('(max-width: 600px)').matches) {
-    document.querySelector('.taskSwitches--wrapper').classList.toggle('slide');
-    setTimeout(() => {
-      switchBtnCompleted.classList.toggle('active');
-      switchBtnActive.classList.toggle('active');
-    }, 200); //Toggle Task Button Animation
+    //Only if on Mobile Devices
+    toggleTaskButtons();
+    toggleTaskContent();
   }
+};
+
+const toggleTaskButtons = () => {
+  document.querySelector('.taskSwitches--wrapper').classList.toggle('slide');
+  setTimeout(() => {
+    switchBtnCompleted.classList.toggle('active');
+    switchBtnActive.classList.toggle('active');
+  }, 200); //Delays Task Button Animation
+};
+
+const toggleTaskContent = () => {
+  //Toggle Tasks on Mobile Devices
+  taskEl.forEach((el) => {
+    el.classList.toggle('shift');
+  });
 };
 
 switchBtnActive.addEventListener('click', toggleTask);
