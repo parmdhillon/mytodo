bc05d79b4d768fdeb68c3f142cc85fdc2d788994
Parminder <iparm1996@gmail.com>
2020-09-17 20:42:16 -0400

    Added Local Storage Support

--- js/app.js
+++ js/app.js
@@ -7,7 +7,7 @@ const addTaskModalBtn = document.getElementById('addTaskModalBtn'); //Display Mo
 const addTaskModal = document.getElementById('addTaskModal');
 const addTaskForm = document.getElementById('addTaskForm'); //Adds Task to the List
 const activeTaskList = document.getElementById('activeTaskList');
-
+const completedTaskList = document.getElementById('completedTaskList');
 // Toggles Tasks on Mobile Devices
 const toggleTask = () => {
   if (window.matchMedia('(max-width: 600px)').matches) {
@@ -55,16 +55,10 @@ const addTaskToList = (e) => {
     taskNameEl.nextElementSibling.style.display = 'block'; //Display Error Message
     return;
   }
-  const taskEl = document.createElement('div');
-  taskEl.classList.add('task--content', 'active');
-  const taskTemplate = `
-      <span>${taskName}</span>
-      <div class="actions">
-        <button class="button small">View Task</button>
-        <button class="button small danger">Delete</button>
-      </div>`;
-  taskEl.innerHTML = taskTemplate;
-  activeTaskList.insertAdjacentElement('beforeend', taskEl);
+  const newTask = new createTask(taskName, 'active', 'TEMP');
+  newTask.storeTask();
+  newTask.updatedUI();
+
   hideAddTaskModal();
   taskNameEl.value = '';
 };
@@ -81,3 +75,94 @@ for (modalOverlay of modalOverlays) {
 for (modalCloseBtn of modalCloseBtns) {
   modalCloseBtn.addEventListener('click', closeModal);
 }
+const setDefaultTaks = () => {
+  if (!localStorage.getItem('allTasks')) {
+    let setDefaultTasks = {
+      tasks: [
+        {
+          taskName: 'House Cleaning',
+          taskStatus: 'active',
+          taskID: 1,
+          taskInfo: 'I have to clean the whole house today till 10AM',
+        },
+        {
+          taskName: 'GitHub Project',
+          taskStatus: 'completed',
+          taskID: 2,
+          taskInfo: 'I have completed MyToDo Project on GitHub',
+        },
+      ],
+    };
+    localStorage.setItem('allTasks', JSON.stringify(setDefaultTasks));
+  }
+};
+
+const loadTasks = () => {
+  storedTasks = JSON.parse(localStorage.getItem('allTasks'));
+  for (task of storedTasks.tasks) {
+    let newTask = new createTask(
+      task.taskName,
+      task.taskStatus,
+      task.taskID,
+      task.taskInfo
+    );
+    newTask.updatedUI();
+  }
+};
+
+class createTask {
+  constructor(name, status, info, id = false) {
+    this.taskName = name;
+    this.taskStatus = status;
+    this.taskInfo = info;
+    this.taskID = id || Math.random() + 1;
+  }
+
+  storeTask() {
+    let storedTasks;
+    if (localStorage.getItem('allTasks')) {
+      storedTasks = JSON.parse(localStorage.getItem('allTasks'));
+    } else {
+      storedTasks = { tasks: [] };
+    }
+    storedTasks.tasks.push({
+      taskName: this.taskName,
+      taskStatus: this.taskStatus,
+      taskID: this.taskID,
+      taskInfo: this.taskInfo,
+    });
+    localStorage.setItem('allTasks', JSON.stringify(storedTasks));
+  }
+
+  updatedUI() {
+    const taskEl = document.createElement('div');
+    const TASK_ACTIVE = 'active';
+    const TASK_COMPLETED = 'completed';
+    if (this.taskStatus == TASK_ACTIVE) {
+      taskEl.classList.add('task--content', TASK_ACTIVE);
+    } else {
+      taskEl.classList.add('task--content', TASK_COMPLETED);
+    }
+    const taskTemplate = `
+        <span>${this.taskName}</span>
+        <div class="actions">
+          <button class="button small">View Task</button>
+          <button class="button small danger">Delete</button>
+        </div>`;
+    taskEl.innerHTML = taskTemplate;
+    if (this.taskStatus == TASK_ACTIVE) {
+      activeTaskList.insertAdjacentElement('beforeend', taskEl);
+    } else {
+      completedTaskList.insertAdjacentElement('beforeend', taskEl);
+    }
+  }
+}
+
+class App {
+  static init() {
+    setDefaultTaks();
+    loadTasks();
+  }
+}
+
+App.init();
